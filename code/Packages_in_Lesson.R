a30421d228f08388c360fa77cf89997dd6416d4a
avven1re <keithfez@gmail.com>
2019-10-14 20:04:16 +0800

    packages for HDDA

--- code/Packages_in_Lesson.R
+++ code/Packages_in_Lesson.R
@@ -1,14 +1,14 @@
 #packages for preparing
 
+#A05 ppt
+browseURL("http://www.hmwu.idv.tw/web/R/A05-hmwu_R-Graphics&Visualization.pdf")
+
 #Colors
-install.packages("RColorBrewer")  #at 47/208
+install.packages("RColorBrewer")  #at 47/208 RColorBrewer
 library(RColorBrewer)
 
-install.packages("fields")        #at 50/208
+install.packages("fields")        #at 50/208 fields
 library(fields)
 
-<<<<<<< HEAD
-install.packages("corrplot")      #at 52/208
+install.packages("corrplot")      #at 52/208 CorrPlot
 library(corrplot)
-=======
->>>>>>> 07da9db4f898ddd9e2f36a50cecd4a38c1db4f91
