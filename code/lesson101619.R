1961cad71e93c1cc3820c6d1aec14adb38a9906b
avven1rre <keithfez@gmail.com>
2019-10-16 10:13:55 +0800

    update

--- code/lesson101619.R
+++ code/lesson101619.R
@@ -1,5 +1,7 @@
 ##101619
+#E04
 
+#8/47 ggplot & 頁面分割
 install.packages("gridExtra")
 library(gridExtra)
 h1 <- ggplot(data=iris, aes(x=Sepal.Length)) + geom_histogram()
@@ -8,3 +10,26 @@ h3 <- ggplot(data=iris, aes(x=Sepal.Length)) +
   geom_histogram(color="black", fill="blue", bins = 10)
 h4 <- ggplot(data=iris, aes(x=Sepal.Length, color=Species)) + geom_histogram(binwidth = 1)
 grid.arrange(h1, h2, h3, h4, nrow=1, ncol=4)
+
+#9/47 geom_histogram
+p <- ggplot(data=iris, aes(x=Sepal.Length))
+p <- p + geom_histogram()
+p + facet_grid(Species~.)
+
+# geom_density
+ggplot(iris, aes(x=Sepal.Length)) + geom_density()
+ggplot(iris, aes(x=Sepal.Length, color=Species)) + geom_density()
+?geom_density
+
+#12~13/47 geom_bar
+p <- ggplot(mtcars, aes(x= cyl)) + geom_bar()
+p
+ggplot(mtcars, aes(x= cyl)) + geom_bar() + coord_flip()
+p + labs(title = "Motor Trend Car Road Tests Data",
+           x = "Number of cylinders", y = "Number of cars")
+
+iris.mean <- aggregate(iris[,1:4], by=list(Species=iris$Species), FUN=mean)
+iris.mean     #aggregate 以Species 為根據做平均
+mydata <- cbind(stack(iris.mean[,-1]), Species = iris.mean$Species)
+mydata
+ggplot(mydata, aes(x=ind, y=values, fill = Species)) + geom_bar(position="dodge", stat="identity")
